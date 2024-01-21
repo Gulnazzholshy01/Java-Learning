4a8bd0dce5d879c8cf8fb31f6b96bf640d868087
Gulnazzholshy01 <gulnaz.zholshy@yandex.kz>
2024-01-21 17:28:19 -0600

    added Loop.java file

---
+++ src/Loop.java
@@ -0,0 +1,15 @@
+public class Loop {
+    public static void main (String[] args) throws Exception {
+
+        //DECLARATIONS 
+        int x;
+        int y = 5;
+
+        //Initialize x
+        x = 0;
+        while (x <= y) {
+            System.out.println(x);
+            x++;
+        }
+    }
+}
